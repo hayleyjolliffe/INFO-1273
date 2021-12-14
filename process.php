bc69c3a5b434775a0fa32df3b960641e7c7cb3c7
hayley jolliffe <93291089+hayleyjolliffe@users.noreply.github.com>
2021-12-14 17:45:59 -0500

    Adding questionable things

--- process.php
+++ process.php
@@ -19,3 +19,16 @@ if(isset($_POST['submit']))
     }
 }
 ?>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
