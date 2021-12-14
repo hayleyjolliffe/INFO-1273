da9d7e2514ed8662ec5288aba2dceeec0b552028
hayley jolliffe <93291089+hayleyjolliffe@users.noreply.github.com>
2021-12-14 17:35:51 -0500

    Updated process.php

--- process.php
+++ process.php
@@ -1,1 +1,21 @@
-// PHP GOES HERE
+<?php
+if(isset($_POST['submit']))
+{
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $pw = $_POST['pw'];
+    $email = $_POST['email'];
+    $address = $_POST['address'];
+    $city = $_POST['city'];
+    $country = $_POST['country'];
+    $result = mysqli_query($mysqli,"insert into user values('','$first_name','$last_name','$pw','$email','$address','$city','$country')");
+    if($result)
+    {
+        echo "Registration successful";
+    }
+    else
+    {
+        echo "Registration failed";
+    }
+}
+?>
